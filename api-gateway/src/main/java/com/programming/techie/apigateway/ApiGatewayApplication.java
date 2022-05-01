84534ca9078327d999ae32122b6ae8f1c6fc51e5
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2022-05-01 22:42:56 +0200

    Part 4- Implement API Gateway

---
+++ api-gateway/src/main/java/com/programming/techie/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.programming.techie.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
