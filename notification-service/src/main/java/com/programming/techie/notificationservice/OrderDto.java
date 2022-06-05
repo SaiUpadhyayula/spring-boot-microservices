897c6d7c26f988fea320bbf011b51a1b66d88ed7
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2022-06-05 16:10:43 +0200

    Part 8- Event Driven Architecture using Spring Cloud Stream and Kafka

---
+++ notification-service/src/main/java/com/programming/techie/notificationservice/OrderDto.java
@@ -0,0 +1,12 @@
+package com.programming.techie.notificationservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDto {
+    private String orderNumber;
+}
--- order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.programmingtechie.orderservice.service;
 
 import com.programmingtechie.orderservice.dto.InventoryResponse;
+import com.programmingtechie.orderservice.dto.OrderDto;
 import com.programmingtechie.orderservice.dto.OrderLineItemsDto;
 import com.programmingtechie.orderservice.dto.OrderRequest;
 import com.programmingtechie.orderservice.model.Order;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -27,6 +30,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
+    private final StreamBridge streamBridge;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -47,21 +51,24 @@ public class OrderService {
 
         Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
 
-        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())){
+        try (Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())) {
             // Call Inventory Service, and place order if product is in
             // stock
-            InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
+            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
                     .uri("http://inventory-service/api/inventory",
                             uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                     .retrieve()
                     .bodyToMono(InventoryResponse[].class)
                     .block();
 
-            boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
+            boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
                     .allMatch(InventoryResponse::isInStock);
 
-            if(allProductsInStock){
+            if (allProductsInStock) {
                 orderRepository.save(order);
+
+                streamBridge.send("notificationEventSupplier-out-0",
+                        MessageBuilder.withPayload(new OrderDto(order.getOrderNumber())).build());
                 return "Order Placed Successfully";
             } else {
                 throw new IllegalArgumentException("Product is not in stock, please try again later");
