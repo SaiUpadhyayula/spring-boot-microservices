27c9ca3d445cbee12b318fb8c8515a2a51bb52d0
saiupadhyayula <subramanyam.sai90@gmail.com>
2023-12-30 18:05:54 +0530

    Update to Spring Boot 3.2

--- api-gateway/src/main/java/com/programming/techie/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/programming/techie/apigateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.programming.techie.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -13,13 +14,13 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
-                .csrf().disable()
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange ->
                         exchange.pathMatchers("/eureka/**")
                                 .permitAll()
                                 .anyExchange()
                                 .authenticated())
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()));
         return serverHttpSecurity.build();
     }
 }
